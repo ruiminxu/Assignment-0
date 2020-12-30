a4c62379e842069815f31e02bcae6a051980710a
ruiminxu <64786097+ruiminxu@users.noreply.github.com>
2020-12-30 16:20:35 -0500

    Assignment - 0

---
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -0,0 +1,15 @@
+function countOfAllBooleansAndStrings(arr) {
+  let count = 0;
+
+  for(let i = 0; i < arr.length; i++)
+  {
+    if(typeof(arr[i]) === 'boolean' || typeof(arr[i]) === 'string')
+    {
+      count++;
+    }
+  }
+  return count;
+}
+
+// Do not edit this line;
+module.exports = countOfAllBooleansAndStrings;
