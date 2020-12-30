896b03958debdc108a6a12e4fcdebeec3c816c9d
ruiminxu <64786097+ruiminxu@users.noreply.github.com>
2020-12-30 16:20:13 -0500

    Assignment - 0

---
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -0,0 +1,18 @@
+function countOfAllBooleans(arr) {
+  let count = 0;
+
+  for(let i = 0; i < arr.length; i++)
+  {
+    if(typeof(arr[i]) ===  'boolean')
+    {
+        count++;
+    }
+  }
+
+  return count;
+
+}
+
+
+// Do not edit this line;
+module.exports = countOfAllBooleans;
