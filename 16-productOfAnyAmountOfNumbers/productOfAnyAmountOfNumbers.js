96fb1c157e3f5beabf20f9bf366a1d49edfdb21b
ruiminxu <64786097+ruiminxu@users.noreply.github.com>
2020-12-30 16:23:05 -0500

    Assignment - 0

---
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -0,0 +1,13 @@
+function productOfAnyAmountOfNumbers(...args) {
+  let total = 1;
+
+  args.forEach(element => total *= element);
+
+  return total;
+
+}
+
+
+
+// Do not edit this line;
+module.exports = productOfAnyAmountOfNumbers;
