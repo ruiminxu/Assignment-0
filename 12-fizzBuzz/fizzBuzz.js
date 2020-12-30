aebd6ed9b95292e873f5ae12d936a67b1c5468dc
ruiminxu <64786097+ruiminxu@users.noreply.github.com>
2020-12-30 16:22:12 -0500

    Assignment - 0

---
+++ 12-fizzBuzz/fizzBuzz.js
@@ -0,0 +1,29 @@
+function fizzBuzz(start, end) {
+  
+  const array = new Array();
+
+  for(let i = start; i <= end; i++)
+  {
+      if(i % 3 === 0 && i % 5 === 0)
+      {
+        array.push("FizzBuzz");
+      }else if(i % 3 === 0)
+      {
+        array.push("Fizz");
+      }else if(i % 5 === 0)
+      {
+        array.push("Buzz");
+      }else{
+        array.push(i);
+      }
+  }
+
+  return array;
+
+}
+
+
+
+
+// Do not edit this line;
+module.exports = fizzBuzz;
